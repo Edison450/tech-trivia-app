81b3f3494d53562eced507d4f33455dd6a3fcb3a
Isaac Ejidokun <ejidokunisaac004@gmail.com>
2025-11-16 07:10:26 +0600

    made the first page interactive

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-console.log('hello');
+function checkAnswer(button) {
+    const correctAnswer = "A";
+    if ( button.id === correctAnswer) {
+        button.style.backgroundColor = "green"
+        button.style.color = "white"
+    } else {
+        button.style.backgroundColor = "red"
+        button.style.color = "white"
+    }
 
-const name = click();
-function click() {
-    console.log(document.querySelector('.js-click').innerHTML);
+    const allButtons = document.querySelectorAll(".option-a");
+        allButtons.forEach(btn => btn.disabled = true);
 }
-
-
